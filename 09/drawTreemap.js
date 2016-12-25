51a0edadcee353b3e33400ef12c68169f4d4ce11
Micah Stubbs <micah.stubbs@gmail.com>
2016-12-24 20:09:14 -0800

    replace jQuery extend with standalone deepExtend function

--- 09/drawTreemap.js
+++ 09/drawTreemap.js
@@ -16,7 +16,7 @@ const defaults = {
 
 function main(o, data) {
   let root;
-  const opts = $.extend(true, {}, defaults, o);
+  const opts = deepExtend({}, defaults, o);
   const formatNumber = d3.format(opts.format);
   const rname = opts.rootname;
   const margin = opts.margin;
@@ -254,6 +254,26 @@ function main(o, data) {
     }
     return `${d.key} (${formatNumber(d.value)})`;
   }
+
+  // a replacement for the jQuery `$.extend(true, {}, objA, objB);`
+  // http://youmightnotneedjquery.com/ with search term `extend`
+  function deepExtend(out) {
+    out = out || {};
+    for (var i = 1; i < arguments.length; i++) {
+      var obj = arguments[i];
+      if (!obj)
+        continue;
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (typeof obj[key] === 'object')
+            out[key] = deepExtend(out[key], obj[key]);
+          else
+            out[key] = obj[key];
+        }
+      }
+    }
+    return out;
+  };
 }
 
 if (window.location.hash === '') {
